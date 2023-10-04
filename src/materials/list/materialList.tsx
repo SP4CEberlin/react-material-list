b8611f7d4834fce7c87c5080b3fdb9fb48d068cb
Oliver Brosch <oliver.brosch@gmail.com>
2023-10-04 10:12:33 +0200

    Add basic structure.

---
+++ src/materials/list/materialList.tsx
@@ -0,0 +1,10 @@
+import {Container} from "react-bootstrap";
+
+export function MaterialList() {
+    return (
+        <Container>
+            Material list
+        </Container>
+    )
+}
+export default MaterialList;

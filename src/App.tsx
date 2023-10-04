795c3c81840110cbb088b7080c87aad71b90a10a
Oliver Brosch <oliver.brosch@gmail.com>
2023-10-04 09:35:26 +0200

    Create a react project.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  // todo:
+  //  load .json-file
+  //  add model
+  //  add add list
+  //  add edit form
+  return (
+    <div className="App">
+      <h1>Welcome to LUM material list.</h1>
+    </div>
+  );
+}
+
+export default App;
